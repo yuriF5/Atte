ef4d15e608a635b2c812a51eabcf7d5372dbf8ab
yuri <arc.bunny.121@gmail.com>
2024-04-01 22:50:52 +0900

    機能追加

--- src/app/Http/Controllers/RegisteredUserController.php
+++ src/app/Http/Controllers/RegisteredUserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisteredUserController extends Controller
 {
