99cf7b1a2c18135aa3a92fc633d5f05d72830427
yuri <arc.bunny.121@gmail.com>
2024-04-07 19:22:29 +0900

    register記述追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Work_timeController;
 use App\Http\Controllers\Rest_timeController;
 use App\Http\Controllers\MiddlewareController;
 
-// ログイン中で勤怠打刻
 Route::group(['middleware' => 'auth'], function() {
     Route::get('/', [RegisteredUserController::class, 'index']); 
     Route::post('/start', [Work_timeController::class,'create'])->name('work_time/start');
@@ -15,13 +14,10 @@ Route::group(['middleware' => 'auth'], function() {
     Route::post('/startRest', [Work_timeController::class,'startRest'])->name('startRest');
     Route::post('/finishRest', [Work_timeController::class,'finishRest'])->name('finishRest');
     Route::get('/attendance', [Work_timeController::class, 'attendance']);
-    
 });
-// ログアウト
 Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->middleware('auth')
     ->name('logout');
-
-// 日別一覧previewボタン用
-Route::post('/attendance/date', [AttendanceController::class, 'perDate'])
+Route::post('/attendance/date', [Work_timeController::class, 'perDate'])
     ->name('per/date');
+Route::post('/register',[RegisteredUserController::class, 'store']);
